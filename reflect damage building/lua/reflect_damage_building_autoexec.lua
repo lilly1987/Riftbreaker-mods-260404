db6750e5d7c7b4597974e74e23bfb6f3de4c58f9
lilly <lilly1987@naver.com>
2026-04-08 00:09:47 +0900

    .

--- reflect damage building/lua/reflect_damage_building_autoexec.lua
+++ reflect damage building/lua/reflect_damage_building_autoexec.lua
@@ -1,56 +1,87 @@
+require("lua/utils/reflection.lua")
 
-RegisterGlobalEventHandler("StartBuildingEvent", function(evt)
-	-- LogService:Log("StartBuildingEvent : " .. tostring(evt:GetEntity() ))
-	-- local currentDifficultyName = DifficultyService:GetCurrentDifficultyName() 
-	local entity=evt:GetEntity() 
-	-- HealthService:SetMaxHealth( entity, HealthService:GetMaxHealth( entity) * 100 )
-end)
+local REFLECT_DAMAGE = {
+	reflect_type = "damage_ratio", -- health_ratio, damage_ratio, flat_damage
+	damage_value = "1",
+	-- melee_only = "1",
+	-- damage_type = "energy",
+}
 
-local function get_or_create_component(blueprint, component_name)
-	local component = blueprint:GetComponent(component_name)
+local REFLECT_HIGHLIGHT = {
+	submesh_idx = "1",
+	glow_factor = "100",
+}
+
+local function set_component_field(component, field_name, value)
+	if ( component == nil or value == nil ) then
+		return
+	end
+
+	local ok, field = pcall(function()
+		return component:GetField(field_name)
+	end)
+	if ( ok and field ~= nil ) then
+		field:SetValue(tostring(value))
+		return
+	end
+
+	local helper = reflection_helper(component)
+	if ( helper ~= nil ) then
+		helper[field_name] = value
+	end
+end
+
+local function get_or_create_entity_component(entity, component_name)
+	local component = EntityService:GetComponent(entity, component_name)
 	if ( component ~= nil ) then
 		return component
 	end
 
-	local ok, created = pcall(function()
-		return blueprint:CreateComponent(component_name)
-	end)
-	if ( ok ) then
-		component = blueprint:GetComponent(component_name)
-		if ( component ~= nil ) then
-			return component
-		end
-		if ( created ~= nil and created ~= true ) then
-			return created
-		end
-	end
-
-	ok, created = pcall(function()
-		return blueprint:AddComponent(component_name)
-	end)
-	if ( ok ) then
-		component = blueprint:GetComponent(component_name)
-		if ( component ~= nil ) then
-			return component
-		end
-		if ( created ~= nil and created ~= true ) then
-			return created
-		end
-	end
-
-	return nil
+	return EntityService:CreateComponent(entity, component_name)
 end
 
-local function set_component_field(component, field_name, value)
-	if ( component == nil ) then
+local function is_player_owned(entity)
+	return EntityService:GetTeam(entity) == EntityService:GetTeam("player")
+end
+
+local function apply_reflect_damage(entity)
+	if ( entity == nil or entity == INVALID_ID ) then
 		return
 	end
 
-	local field = component:GetField(field_name)
-	if ( field ~= nil ) then
-		field:SetValue(tostring(value))
+	if ( not EntityService:IsAlive(entity) ) then
+		return
+	end
+
+	if ( not is_player_owned(entity) ) then
+		return
+	end
+
+	local reflect_damage_component = get_or_create_entity_component(entity, "ReflectDamageComponent")
+	if ( reflect_damage_component ~= nil ) then
+		set_component_field(reflect_damage_component, "reflect_type", REFLECT_DAMAGE.reflect_type)
+		set_component_field(reflect_damage_component, "melee_only", REFLECT_DAMAGE.melee_only)
+		set_component_field(reflect_damage_component, "damage_value", REFLECT_DAMAGE.damage_value)
+		set_component_field(reflect_damage_component, "damage_type", REFLECT_DAMAGE.damage_type)
+	else
+		LogService:Log("ReflectDamageComponent create fail : " .. tostring(entity))
+	end
+
+	local reflect_highlight_component = get_or_create_entity_component(entity, "ReflectHighlightComponent")
+	if ( reflect_highlight_component ~= nil ) then
+		set_component_field(reflect_highlight_component, "submesh_idx", REFLECT_HIGHLIGHT.submesh_idx)
+		set_component_field(reflect_highlight_component, "glow_factor", REFLECT_HIGHLIGHT.glow_factor)
+	else
+		LogService:Log("ReflectHighlightComponent create fail : " .. tostring(entity))
 	end
 end
+
+RegisterGlobalEventHandler("StartBuildingEvent", function(evt)
+	local entity = evt:GetEntity()
+	apply_reflect_damage(entity)
+	ConsoleService:Write("StartBuildingEvent : " .. tostring(entity))
+	LogService:Log("StartBuildingEvent : " .. tostring(entity))
+end)
 
 RegisterGlobalEventHandler("MissionFlowDeactivatedEvent", function(evt)
 	-----------------------------------------------
@@ -59,7 +90,7 @@ RegisterGlobalEventHandler("MissionFlowDeactivatedEvent", function(evt)
         LogService:Log("database no")
         return
     end
-	local k1="reflect_damage_building_autoexec.lua/" 
+	local k1="reflect_damage_building_autoexec.lua/"
 	if database:HasInt( k1) then
 		LogService:Log(" database has " )
 		return
@@ -69,68 +100,9 @@ RegisterGlobalEventHandler("MissionFlowDeactivatedEvent", function(evt)
 	end
 	-----------------------------------------------
 	local entities = FindService:FindEntitiesByType( "building" )
+	ConsoleService:Write("FindEntitiesByType building : " .. tostring(#entities))
+	LogService:Log("FindEntitiesByType building : " .. tostring(#entities))
 	for _,entity in pairs( entities ) do
-		-- HealthService:SetMaxHealth( entity, HealthService:GetMaxHealth( entity) * 100 )
+		apply_reflect_damage(entity)
 	end
-end)
-
-local setup=function(blueprint_name)
-	LogService:Log(blueprint_name)
-	
-	local blueprint = ResourceManager:GetBlueprint(blueprint_name)
-	if ( blueprint == nil ) then
-		LogService:Log(" blueprint no : " .. blueprint_name)
-		return
-	end
-	
-	local reflect_damage_component = get_or_create_component(blueprint, "ReflectDamageComponent")
-	if ( reflect_damage_component == nil ) then
-		LogService:Log(" ReflectDamageComponent create fail : " .. blueprint_name)
-		return
-	end
-	
-	set_component_field(reflect_damage_component, "reflect_type", "damage_ratio") -- health_ratio, damage_ratio, flat_damage
-	-- set_component_field(reflect_damage_component, "melee_only", "1")
-	set_component_field(reflect_damage_component, "damage_value", "1")
-	-- set_component_field(reflect_damage_component, "damage_type", "energy")
-	
-	local reflect_highlight_component = get_or_create_component(blueprint, "ReflectHighlightComponent")
-	if ( reflect_highlight_component ~= nil ) then
-		set_component_field(reflect_highlight_component, "submesh_idx", "1")
-		set_component_field(reflect_highlight_component, "glow_factor", "100")
-	else
-		LogService:Log(" ReflectHighlightComponent create fail : " .. blueprint_name)
-	end
-	
-	--local buildingComponent = blueprint:GetComponent("BuildingComponent")		
-	--local type = buildingComponent:GetField("type"):GetValue())
-	--LogService:Log(" type : " .. type)
-	
-	-- local TypeComponent = blueprint:GetComponent("TypeComponent")		
-	-- local type = TypeComponent:GetField("type"):GetValue()
-	-- LogService:Log(" type : " .. type)
-	
-	-- local max_health=0
-	-- local health=0
-	
-	-- local HealthDesc = blueprint:GetComponent("HealthDesc")	
-	-- if ( HealthDesc == nil ) then
-	-- 	LogService:Log(" HealthDesc no ")
-	-- else
-	-- 	max_health = HealthDesc:GetField("max_health"):GetValue()
-	-- 	LogService:Log(" max_health : " .. max_health)
-	-- end
-	
-	-- local HealthComponent = blueprint:GetComponent("HealthComponent")	
-	-- if ( HealthComponent == nil ) then
-	-- 	LogService:Log(" HealthComponent no ")
-	-- else
-	-- 	max_health = HealthComponent:GetField("max_health"):GetValue()
-	-- 	LogService:Log(" max_health : " .. max_health)
-	-- end
-end
-
-
-RegisterGlobalEventHandler("PlayerInitializedEvent", function(evt)
-	setup()
 end)
